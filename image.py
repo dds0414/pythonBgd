1ee03388b3c9ed1d660c6d0a0f97357bce672f6f
yangbo <yangbo@21eline.com>
2016-07-12 16:18:49 +0800

    pdf转图片

--- image.py
+++ image.py
@@ -1,7 +1,33 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
 
-def IMGRecognition():
-    pass
 
+def IMGRecognition(IMGPath):
+    image = cv2.imread(IMGPath)
+    print image.shape
+    # grayMat = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # binMat = 255 - grayMat
+    # np.savetxt("a.txt", binMat)
+    # print binMat
+
+# def setImage(image):
+#     # 灰度处理
+#     grayMat = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     # 反转并去噪
+#     binMat = 255 - grayMat
+#     for indexRow, i in enumerate(binMat):
+#         for index, n in enumerate(i):
+#             if n < 25:
+#                 binMat[indexRow][index] = 0
+#     # 去边缘
+#     row, col = guiyi(binMat)
+#     one = binMat[row[0]:row[-1], col[0]:col[-1] + 1]
+#     # resize
+#     res = cv2.resize(one, (30, 32), Image.ANTIALIAS)
+#     # 归一化
+#     data = res.reshape((1, 960))[0]
+#     return data, res
 
 if __name__ == u"__main__":
-    IMGRecognition()
+    IMGRecognition("bgd/test.jpg")
---
+++ test.py
@@ -0,0 +1,9 @@
+from wand.image import Image
+
+with Image(filename='pdf/1.pdf', width=2480, height=3509, resolution=300) as pdf:
+    with pdf.convert('jpg') as image:
+        print('format =', image.format)
+        print('width =', image.width)
+        print('height =', image.height)
+        # image.resize(400, 300)
+        image.save(filename='result1.jpg')
