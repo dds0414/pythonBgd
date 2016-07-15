35dd6cabe0b2dc0fb1d35f07a58e0a56cee50206
yangbo <yangbo@21eline.com>
2016-07-15 13:42:05 +0800

    pdf转图片

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 from wand.image import Image
 
-with Image(filename='pdf/1.pdf', width=2480, height=3509, resolution=300) as pdf:
+with Image(filename='pdf/2.pdf', resolution=300) as pdf:
     with pdf.convert('jpg') as image:
-        print('format =', image.format)
-        print('width =', image.width)
-        print('height =', image.height)
-        image.save(filename='result1.jpg')
+        image.save(filename='result2.jpg')
