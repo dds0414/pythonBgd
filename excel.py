8ce23d84d74e69d8a204bcd46e7909eb192e0682
yangbo <yangbo@21eline.com>
2016-07-18 16:35:20 +0800

    excel + word

--- excel.py
+++ excel.py
@@ -1,12 +1,22 @@
 # -*- encoding: utf8 -*-
 import xlrd
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 data = xlrd.open_workbook('excel/1.xls')
 
 # 通过索引顺序获取
 table = data.sheets()[0]
+# table = data.data.sheet_by_index(0)
 row = table.nrows
 col = table.ncols
 for i in range(row):
-    for v in table.row_values(i):
-        print v
+    # print table.row_values(i)
+    for key, val in enumerate(table.row_values(i)):
+        if u'成交方式' in str(val).decode():
+            print u'成交方式', table.row_values(i+1)[key]
+        if u'运抵国(地区)' in str(val).decode():
+            print u'运抵国(地区)', table.row_values(i + 1)[key]
+        if u'贸易方式' in str(val).decode():
+            print u'贸易方式', table.row_values(i + 1)[key]
--- word.py
+++ word.py
@@ -5,4 +5,4 @@ from xml.dom import minidom
 document = zipfile.ZipFile('word/4.docx')
 xml_content = document.read('word/document.xml')
 reparsed = minidom.parseString(xml_content)
-print reparsed.toprettyxml(indent="     ", encoding="utf-8")
+print reparsed.toprettyxml(indent="  ", encoding="utf-8")
