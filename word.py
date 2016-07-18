74c7ef8dd37917d9bd9223c3e998631a62703b55
yangbo <yangbo@21eline.com>
2016-07-18 15:25:57 +0800

    excel + word

--- word.py
+++ word.py
@@ -2,7 +2,7 @@
 import zipfile
 from xml.dom import minidom
 
-document = zipfile.ZipFile('word/1.docx')
+document = zipfile.ZipFile('word/4.docx')
 xml_content = document.read('word/document.xml')
 reparsed = minidom.parseString(xml_content)
 print reparsed.toprettyxml(indent="     ", encoding="utf-8")
