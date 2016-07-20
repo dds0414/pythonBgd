7517745947314e7ac11cd73ca8018716254b75ce
yangbo <yangbo@21eline.com>
2016-07-20 15:53:34 +0800

    cnn

--- excel.py
+++ excel.py
@@ -6,17 +6,14 @@ sys.setdefaultencoding("utf-8")
 
 data = xlrd.open_workbook('excel/1.xls')
 
-# 通过索引顺序获取
 table = data.sheets()[0]
-# table = data.data.sheet_by_index(0)
 row = table.nrows
 col = table.ncols
 for i in range(row):
-    # print table.row_values(i)
     for key, val in enumerate(table.row_values(i)):
-        if u'成交方式' in str(val).decode():
-            print u'成交方式', table.row_values(i+1)[key]
-        if u'运抵国(地区)' in str(val).decode():
+        if u'成交方式' in str(val):
+            print u'成交方式', table.row_values(i + 1)[key]
+        if u'运抵国(地区)' in str(val):
             print u'运抵国(地区)', table.row_values(i + 1)[key]
-        if u'贸易方式' in str(val).decode():
+        if u'贸易方式' in str(val):
             print u'贸易方式', table.row_values(i + 1)[key]
